520906a393005c89835af2e88f7523b1a87b0f4c
SANTOSH KUMAR SAH <104618650+santoshkrsah@users.noreply.github.com>
2024-09-02 06:17:24 +0530

    Update webpack.config.js

--- anantclasses.in_oceanwp/webpack.config.js
+++ anantclasses.in_oceanwp/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path'); 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const CopyPlugin = require("copy-webpack-plugin");
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
